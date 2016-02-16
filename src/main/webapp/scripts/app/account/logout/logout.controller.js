d3911c75e6409935e618b11c4ae9ca0cbbbed054
Francesco Uliana <francesco.uliana@cnr.it>
2016-02-16 11:40:35 +0100

    cleanup

--- src/main/webapp/scripts/app/account/logout/logout.controller.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('missioniApp')
-    .controller('LogoutController', function (Auth) {
-        Auth.logout();
-    });
--- src/main/webapp/scripts/components/auth/services/register.service.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('missioniApp')
-    .factory('Register', function ($resource) {
-        return $resource('api/register', {}, {
-        });
-    });
-
-
