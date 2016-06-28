40ef2d01cf9e87e2ee7aa7c7b47de9bbd81bbce5
Francesco Uliana <francesco.uliana@cnr.it>
2016-06-28 09:35:10 +0200

    ApplicationTest

--- src/test/java/it/cnr/si/ApplicationTest.java
+++ src/test/java/it/cnr/si/ApplicationTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
-public class ApplicationTests {
+public class ApplicationTest {
 
 	@Test
 	public void contextLoads() {
