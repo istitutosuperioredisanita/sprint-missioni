a6e85261d07d96b143c7fc72c6cddc8f2546499b
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2016-07-05 14:44:26 +0200

    Work-in-progress

---
+++ src/main/java/it/cnr/si/missioni/service/RimborsoMissioneService.java
@@ -0,0 +1,16 @@
+package it.cnr.si.missioni.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * Service class for managing users.
+ */
+@Service
+public class RimborsoMissioneService {
+
+    private final Logger log = LoggerFactory.getLogger(RimborsoMissioneService.class);
+
+}
---
+++ src/main/java/it/cnr/si/missioni/web/filter/RimborsoMissioneFilter.java
@@ -0,0 +1,6 @@
+package it.cnr.si.missioni.web.filter;
+
+import java.util.Date;
+
+public class RimborsoMissioneFilter extends OrdineMissioneFilter {
+}
