babc87a1421012619d53db06082c330d0ab15450
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2017-04-12 16:32:14 +0200

    Aggiunta gestione code

---
+++ src/main/java/it/cnr/si/missioni/amq/domain/TypeMissione.java
@@ -0,0 +1,25 @@
+package it.cnr.si.missioni.amq.domain;
+
+public enum TypeMissione {
+	ORDINE("Ordine"),
+	RIMBORSO("Rimborso");
+	
+	private final String value;
+
+	private TypeMissione(String value) {
+		this.value = value;
+	}
+
+	public String value() {
+		return value;
+	}
+
+	public static TypeMissione fromValue(String v) {
+		for (TypeMissione c : TypeMissione.values()) {
+			if (c.value.equals(v)) {
+				return c;
+			}
+		}
+		throw new IllegalArgumentException(v);
+	}	
+}
