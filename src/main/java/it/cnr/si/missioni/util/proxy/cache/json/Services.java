e3abefdd2f0a1aea75892fd22a2a5751ae1f7c5e
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2017-09-06 14:29:45 +0200

    Corretta anomalia che faceva fare richieste ad Alfresco di continuo per caricare i file di configurazione

--- src/main/java/it/cnr/si/missioni/util/proxy/cache/json/Services.java
+++ src/main/java/it/cnr/si/missioni/util/proxy/cache/json/Services.java
@@ -1,29 +1,32 @@
 package it.cnr.si.missioni.util.proxy.cache.json;
 
-import java.io.Serializable;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
 import javax.annotation.Generated;
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.hazelcast.nio.ObjectDataInput;
+import com.hazelcast.nio.ObjectDataOutput;
+import com.hazelcast.nio.serialization.DataSerializable;
+
+import it.cnr.si.missioni.awesome.exception.AwesomeException;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Generated("org.jsonschema2pojo")
 @JsonPropertyOrder({
 	"restService"
 })
-public class Services implements Serializable{
+public class Services implements DataSerializable{
 
 	@JsonProperty("restService")
 	private List<RestService> restService = new ArrayList<RestService>();
-	@JsonIgnore
-	private Map<String, Object> additionalProperties = new HashMap<String, Object>();
 
 	/**
 	 *
@@ -45,14 +48,28 @@ public class Services implements Serializable{
 		this.restService = restService;
 	}
 
-	@JsonAnyGetter
-	public Map<String, Object> getAdditionalProperties() {
-		return this.additionalProperties;
+	@Override
+	public String toString() {
+		return "Services [RestServices=" + restService + "]";
 	}
-
-	@JsonAnySetter
-	public void setAdditionalProperty(String name, Object value) {
-		this.additionalProperties.put(name, value);
+	public String getMd5() {
+		String objectToString = toString();
+		MessageDigest m = null;
+		try {
+			m = MessageDigest.getInstance("MD5");
+		    m.update(objectToString.getBytes(),0,objectToString.length());
+		    return new BigInteger(1,m.digest()).toString(16);
+		} catch (NoSuchAlgorithmException e) {
+			new AwesomeException("Errore nel recupero dell'algoritmo MD5 "+e.getMessage());
+		}
+		return "";
 	}
-
+	@Override
+	public void writeData(ObjectDataOutput out) throws IOException {
+		out.writeUTF(toString());
+	}
+	@Override
+	public void readData(ObjectDataInput in) throws IOException {
+		in.readData();
+	}
 }
