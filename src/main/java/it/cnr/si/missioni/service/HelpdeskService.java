c7d923c68095d0d1132a63f3cd3110058598c1e6
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2018-02-13 10:40:27 +0100

    Create classi Helpdesk

---
+++ src/main/java/it/cnr/si/missioni/service/HelpdeskService.java
@@ -0,0 +1,126 @@
+package it.cnr.si.missioni.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpStatus;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.AuthenticationException;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.hibernate.service.spi.ServiceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+
+import it.cnr.si.missioni.awesome.exception.AwesomeException;
+import it.cnr.si.missioni.domain.custom.Helpdesk;
+
+public class HelpdeskService {
+    private final Logger log = LoggerFactory.getLogger(HelpdeskService.class);
+
+    @Value("${helpdesk.username}")
+    private static String helpdeskUsername;
+
+    @Value("${helpdesk.password}")
+    private static String helpdeskPassword;
+
+    @Value("${helpdesk.url}")
+    private static String helpdeskUrl;
+
+	public static long newProblem(Helpdesk hd, String instance) throws ServiceException {
+		CloseableHttpClient client = null;
+		CloseableHttpResponse response = null;
+		long id = -1L;
+		try {
+			CredentialsProvider provider = new BasicCredentialsProvider();
+			UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(helpdeskUsername, helpdeskPassword);
+			provider.setCredentials(AuthScope.ANY, credentials);
+			client = HttpClientBuilder.create().setDefaultCredentialsProvider(provider).build();
+			HttpPut request = new HttpPut(helpdeskUrl+instance);
+			request.addHeader(new BasicScheme().authenticate(credentials, request, null));
+
+			StringEntity entity = new StringEntity(new Gso().toJson(hd), ContentType.APPLICATION_JSON);
+			request.setEntity(entity);		
+			response = client.execute(request);
+			if (HttpStatus.SC_CREATED!=response.getStatusLine().getStatusCode())
+				throw new ServiceException(response.getStatusLine().getStatusCode()+" - "+response.getStatusLine().getReasonPhrase());
+			HttpEntity resEntity = response.getEntity();
+		    if (resEntity != null)
+		    	id = Long.parseLong(EntityUtils.toString(resEntity));
+		    if (resEntity != null) 
+		      EntityUtils.consume(resEntity);
+		} catch (AuthenticationException e) {
+			e.printStackTrace();
+			throw new AwesomeException("Errore nell'autenticazione per l'invio della segnalazione ad OIL: "+e.getLocalizedMessage());
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+			throw new AwesomeException("Errore di protocollo per l'invio della segnalazione ad OIL: "+e.getLocalizedMessage());
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new AwesomeException("Errore IO per l'invio della segnalazione ad OIL: "+e.getLocalizedMessage());
+		} finally {
+			try {
+				if (response!=null) response.close();
+				if (client!=null) client.close();
+			} catch (IOException e) {
+				// Do nothing
+				e.printStackTrace();
+			}
+		}
+		return id;
+	}
+	
+	public static void addAttachments(long id, List<File> files, String instance) throws ServiceException {
+		CloseableHttpClient client = null;
+		CloseableHttpResponse response = null;
+		try {
+			for (File file : files) {
+				String filename = file.getName();
+				System.out.println(FileUtils.readFileToString(file));
+				CredentialsProvider provider = new BasicCredentialsProvider();
+				UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(helpdeskUsername, helpdeskPassword);
+				provider.setCredentials(AuthScope.ANY, credentials);
+				client = HttpClientBuilder.create().setDefaultCredentialsProvider(provider).build();
+				MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+				builder.addBinaryBody(Helpdesk.ALLEGATO, file, ContentType.DEFAULT_BINARY, filename);
+				HttpEntity multipart = builder.build();
+				HttpPost request = new HttpPost(helpdeskUrl+instance+"/"+id);
+				request.addHeader(new BasicScheme().authenticate(credentials, request, null));
+				request.setEntity(multipart);
+				response = client.execute(request);
+				if (HttpStatus.SC_NO_CONTENT!=response.getStatusLine().getStatusCode())
+					System.out.println("Errore nel POST del file: "+filename);
+			}
+		} catch (AuthenticationException e) {
+			e.printStackTrace();
+			throw new AwesomeException("Errore nell'autenticazione per l'invio della segnalazione ad OIL: "+e.getLocalizedMessage());
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new AwesomeException("Errore IO per l'invio della segnalazione ad OIL: "+e.getLocalizedMessage());
+		} finally {
+			try {
+				if (response!=null) response.close();
+				if (client!=null) client.close();
+			} catch (IOException e) {
+				// Do nothing
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
---
+++ src/main/java/it/cnr/si/missioni/web/rest/HelpdeskResource.java
@@ -0,0 +1,49 @@
+package it.cnr.si.missioni.web.rest;
+
+import javax.annotation.security.RolesAllowed;
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import it.cnr.si.missioni.domain.custom.Helpdesk;
+import it.cnr.si.missioni.util.JSONResponseEntity;
+import it.cnr.si.security.AuthoritiesConstants;
+
+@RolesAllowed({AuthoritiesConstants.USER})
+@RestController
+@RequestMapping("/api")
+public class HelpdeskResource {
+	private final Logger log = LoggerFactory.getLogger(HelpdeskResource.class);
+
+	@RequestMapping(value = "/sendWithAttachment", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)	
+	public ResponseEntity sendWithAttachment(HttpServletRequest req, @RequestParam("file") MultipartFile uploadedMultipartFile) {
+		log.debug("HelpdeskResource:send");
+		Helpdesk hd = new Helpdesk();
+		hd.setSubject(req.getParameter("subject"));
+		hd.setMessage(req.getParameter("message"));
+		hd.setCategory(req.getParameter("category"));
+		hd.setDescCategory(req.getParameter("desc-category"));
+		hd.setIp(req.getRemoteAddr());
+		hd.setAllegato(uploadedMultipartFile);
+		
+		helpdeskService.sendMessage(hdDataModel);
+		return JSONResponseEntity.ok();
+	}
+
+	@RequestMapping(value = "/sendWithoutAttachment", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)	
+	public ResponseEntity sendWithoutAttachment(HttpServletRequest req, @RequestBody Helpdesk hdDataModel) {
+		log.debug("HelpdeskResource:send");
+		hdDataModel.setIp(req.getRemoteAddr());
+		helpdeskService.sendMessage(hdDataModel);
+		return JSONResponseEntity.ok();	
+	}
+}
