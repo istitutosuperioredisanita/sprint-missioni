f164b9bfd4e1ecbc3c91220e0e4f7ac24d1514f2
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2018-06-06 12:17:51 +0200

    Aggiunta gestione missioni del presidente

--- src/main/java/it/cnr/si/missioni/domain/custom/persistence/Parametri.java
+++ src/main/java/it/cnr/si/missioni/domain/custom/persistence/Parametri.java
@@ -2,7 +2,6 @@ package it.cnr.si.missioni.domain.custom.persistence;
 
 
 import java.io.Serializable;
-import java.time.LocalDate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -30,6 +29,10 @@ public class Parametri extends OggettoBulkXmlTransient implements Serializable {
     @Column(name = "RESPONSABILE_CUG", length = 200, nullable = true)
     private String responsabileCug;
 
+    @Size(min = 0, max = 200)
+    @Column(name = "PRESIDENTE", length = 200, nullable = true)
+    private String presidente;
+
 	public void setId(Long id) {
 		this.id = id;
 	}
@@ -42,5 +45,11 @@ public class Parametri extends OggettoBulkXmlTransient implements Serializable {
 	}
 	public void setResponsabileCug(String responsabileCug) {
 		this.responsabileCug = responsabileCug;
+	}
+	public String getPresidente() {
+		return presidente;
+	}
+	public void setPresidente(String presidente) {
+		this.presidente = presidente;
 	}
 }
