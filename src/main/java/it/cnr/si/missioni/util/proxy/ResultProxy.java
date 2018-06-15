348844a6faab52349d9e99d9794f0e90e4250c13
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2018-06-15 12:51:52 +0200

    Inseriti LOG per capire errore

--- src/main/java/it/cnr/si/missioni/util/proxy/ResultProxy.java
+++ src/main/java/it/cnr/si/missioni/util/proxy/ResultProxy.java
@@ -4,6 +4,11 @@ import java.io.Serializable;
 
 import org.springframework.http.HttpStatus;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import it.cnr.si.missioni.awesome.exception.AwesomeException;
+import it.cnr.si.missioni.util.CodiciErrore;
+import it.cnr.si.missioni.util.Utility;
 import it.cnr.si.missioni.util.proxy.json.object.CommonJsonRest;
 import it.cnr.si.missioni.util.proxy.json.object.RestServiceBean;
 
@@ -38,6 +43,17 @@ public class ResultProxy implements Serializable {
 			CommonJsonRest<RestServiceBean> commonJsonResponse) {
 		this.commonJsonResponse = commonJsonResponse;
 	}
-
+	@Override
+	public String toString() {
+		String jsonBody = null;
+    	try {
+    		ObjectMapper mapper = new ObjectMapper();
+    		jsonBody = mapper.writeValueAsString(body);
+    	} catch (Exception ex) {
+    		throw new AwesomeException(CodiciErrore.ERRGEN, "Errore nella manipolazione del file JSON per la responde della REST ("+Utility.getMessageException(ex)+").");
+    	}
+		return "CallCache [status=" + status + ", body=" + jsonBody.toString()
+				+ ", type=" + type + ", commonJsonResponse=" + commonJsonResponse;
+	}
 }
 
