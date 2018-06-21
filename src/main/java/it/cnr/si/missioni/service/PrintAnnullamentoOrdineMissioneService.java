4953e6733a74d947d536f102ff7e5dbf3ebbacd0
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2018-06-21 11:12:08 +0200

    Corretto lancio Stampa Annullamento

--- src/main/java/it/cnr/si/missioni/service/PrintAnnullamentoOrdineMissioneService.java
+++ src/main/java/it/cnr/si/missioni/service/PrintAnnullamentoOrdineMissioneService.java
@@ -3,16 +3,24 @@ package it.cnr.si.missioni.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.bind.RelaxedPropertyResolver;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import it.cnr.jada.ejb.session.ComponentException;
 import it.cnr.si.missioni.awesome.exception.AwesomeException;
 import it.cnr.si.missioni.domain.custom.persistence.AnnullamentoOrdineMissione;
 import it.cnr.si.missioni.domain.custom.print.PrintOrdineMissione;
+import it.cnr.si.missioni.util.Costanti;
 
 @Service
 public class PrintAnnullamentoOrdineMissioneService {
     private final Logger log = LoggerFactory.getLogger(PrintAnnullamentoOrdineMissioneService.class);
+
+	@Autowired	
+	private Environment env;
+
+	private RelaxedPropertyResolver propertyResolver;
 
     @Autowired
     private PrintService printService;
@@ -22,7 +30,14 @@ public class PrintAnnullamentoOrdineMissioneService {
 
 	public byte[] printOrdineMissione(AnnullamentoOrdineMissione annullamento, String currentLogin) throws AwesomeException, ComponentException {
 		String myJson = createJsonPrintOrdineMissione(annullamento, currentLogin);
-		return printService.print(myJson, "OrdineMissione.jasper");
+    	this.propertyResolver = new RelaxedPropertyResolver(env, "spring.print.");
+    	String nomeStampa = "";
+    	if (propertyResolver != null && propertyResolver.getProperty(Costanti.NOME_STAMPA_ORDINE) != null) {
+    		nomeStampa = propertyResolver.getProperty(Costanti.NOME_STAMPA_ORDINE);
+    	} else {
+    		throw new ComponentException("Configurare il nome stampa dell'ordine");
+    	}
+		return printService.print(myJson, nomeStampa);
 	}
 
 	public String createJsonPrintOrdineMissione(AnnullamentoOrdineMissione annullamento, String currentLogin) throws ComponentException {
