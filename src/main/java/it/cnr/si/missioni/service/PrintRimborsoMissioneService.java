7582e05a7cd14134f43a5f434c6c5647de133ec0
Gianfranco Gasparro <gianfranco.gasparro@cnr.it>
2018-07-09 14:48:57 +0200

    Fix

--- src/main/java/it/cnr/si/missioni/service/PrintRimborsoMissioneService.java
+++ src/main/java/it/cnr/si/missioni/service/PrintRimborsoMissioneService.java
@@ -4,10 +4,8 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
