18f103b2170cf9a5ed3e9a0a72e40ed91140d8d0
gianfranco.gasparro <gianfranco.gasparro@cnr.it>
2020-11-06 19:50:28 +0100

    Gestito LOG richieste REST

---
+++ src/main/java/it/cnr/si/missioni/config/LoggableDispatcherServlet.java
@@ -0,0 +1,87 @@
+package it.cnr.si.missioni.config;
+
+import it.cnr.si.missioni.service.OrdineMissioneService;
+import it.cnr.si.missioni.util.DateUtils;
+import it.cnr.si.missioni.util.SecurityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.log.LogMessage;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.HandlerExecutionChain;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+import org.springframework.web.util.WebUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.security.Principal;
+import java.util.stream.Collectors;
+
+public class LoggableDispatcherServlet extends DispatcherServlet {
+
+    private final Logger log = LoggerFactory.getLogger(LoggableDispatcherServlet.class);
+
+    @Override
+    protected void doDispatch(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        if (!(request instanceof ContentCachingRequestWrapper)) {
+            request = new ContentCachingRequestWrapper(request);
+        }
+        if (!(response instanceof ContentCachingResponseWrapper)) {
+            response = new ContentCachingResponseWrapper(response);
+        }
+        HandlerExecutionChain handler = getHandler(request);
+
+        try {
+            super.doDispatch(request, response);
+        } finally {
+            log(request, response, handler);
+            updateResponse(response);
+        }
+    }
+
+
+    private void log(HttpServletRequest requestToCache, HttpServletResponse responseToCache, HandlerExecutionChain handler) {
+        String uri = requestToCache.getRequestURI();
+        if (!uri.startsWith("/styles") && !uri.startsWith("/scripts") && !uri.startsWith("/fonts") && !uri.startsWith("/images") && !uri.contains("authentication_check.gif")  && SecurityUtils.getCurrentUser() != null ){
+
+            try {
+                byte[] cachedContent = ((ContentCachingRequestWrapper) requestToCache).getContentAsByteArray();
+                String payload =  "";
+                payload= new String(cachedContent, StandardCharsets.UTF_8);
+                BufferedReader buffer = requestToCache.getReader();
+                logger.info(((Principal) SecurityUtils.getCurrentUser()).getName()+" "+requestToCache.getMethod()+" "+requestToCache.getRequestURI()+" "+requestToCache.getQueryString()+" "+payload+" "+requestToCache.getRemoteAddr());
+            } catch (IOException e) {
+                logger.info(e.getMessage() );
+            }
+        }
+     }
+
+    private String getResponsePayload(HttpServletResponse response) {
+        ContentCachingResponseWrapper wrapper = WebUtils.getNativeResponse(response, ContentCachingResponseWrapper.class);
+        if (wrapper != null) {
+
+            byte[] buf = wrapper.getContentAsByteArray();
+            if (buf.length > 0) {
+                int length = Math.min(buf.length, 5120);
+                try {
+                    return new String(buf, 0, length, wrapper.getCharacterEncoding());
+                }
+                catch (UnsupportedEncodingException ex) {
+                    // NOOP
+                }
+            }
+        }
+        return "[unknown]";
+    }
+
+    private void updateResponse(HttpServletResponse response) throws IOException {
+        ContentCachingResponseWrapper responseWrapper =
+                WebUtils.getNativeResponse(response, ContentCachingResponseWrapper.class);
+        responseWrapper.copyBodyToResponse();
+    }
+        }
