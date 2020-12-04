2ce73f641ea5f30c15c9b6de0f6cb9d64d9b185c
gianfranco.gasparro <gianfranco.gasparro@cnr.it>
2020-12-04 21:45:05 +0100

    Aggiunta gestione Logging Request

--- src/main/java/it/cnr/si/missioni/config/DateTimeFormatConfiguration.java
+++ src/main/java/it/cnr/si/missioni/config/DateTimeFormatConfiguration.java
@@ -1,12 +1,17 @@
 package it.cnr.si.missioni.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class DateTimeFormatConfiguration extends WebMvcConfigurerAdapter {
+    @Autowired
+    MissioniLoggingGet missioniLoggingGet;
+
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
@@ -14,4 +19,10 @@ public class DateTimeFormatConfiguration extends WebMvcConfigurerAdapter {
         registrar.setUseIsoFormat(true);
         registrar.registerFormatters(registry);
     }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(missioniLoggingGet);
+    }
+
 }
