c9419fd2194603edae45db2abc1c9b51f266688d
gianfranco.gasparro <gianfranco.gasparro@cnr.it>
2021-01-05 19:05:26 +0100

    Revert "Fix anomalie varie segnalate su helpdesk"
    This reverts commit ee51106e

--- src/main/java/it/cnr/si/missioni/web/rest/AccountLDAPResource.java
+++ src/main/java/it/cnr/si/missioni/web/rest/AccountLDAPResource.java
@@ -68,7 +68,7 @@ public class AccountLDAPResource {
             produces = MediaType.APPLICATION_JSON_VALUE)
     @Timed
     public ResponseEntity<String> getSiperAccount() {
-        String resp = accountService.getAccount("sonia.vivona", true);
+        String resp = accountService.getAccount(SecurityUtils.getCurrentUserLogin(), true);
         return new ResponseEntity<String>(resp, HttpStatus.OK);
     }
 
