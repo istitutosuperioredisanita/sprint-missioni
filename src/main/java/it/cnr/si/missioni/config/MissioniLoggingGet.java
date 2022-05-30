254e6720f52b2d1280e0c0fc637c7d91e2a0edc4
gianfranco.gasparro <gianfranco.gasparro@cnr.it>
2022-05-30 15:41:05 +0200

    Fix Helpdesk, log

--- src/main/java/it/cnr/si/missioni/config/MissioniLoggingGet.java
+++ src/main/java/it/cnr/si/missioni/config/MissioniLoggingGet.java
@@ -1,6 +1,9 @@
 package it.cnr.si.missioni.config;
 
 import it.cnr.si.missioni.util.SecurityUtils;
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.KeycloakSecurityContext;
+import org.keycloak.representations.IDToken;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpMethod;
@@ -11,6 +14,7 @@ import javax.servlet.DispatcherType;
 import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.security.Principal;
 
 
 @Component
@@ -27,7 +31,12 @@ public class MissioniLoggingGet  extends HandlerInterceptorAdapter {
                 && request.getMethod().equals(HttpMethod.GET.name()) && SecurityUtils.getCurrentUser() != null) {
             String uri = request.getRequestURI();
             if (!uri.startsWith("/styles") && !uri.startsWith("/scripts") && !uri.startsWith("/fonts") && !uri.startsWith("/images") && !uri.contains("authentication_check.gif")  && !uri.endsWith("ico") && !uri.endsWith("png") && SecurityUtils.getCurrentUser() != null ){
-                log.info( SecurityUtils.getCurrentUser()+" "+request.getMethod()+" "+request.getRequestURI()+" "+request.getQueryString()+" "+request.getRemoteAddr());
+                Object principal = (Object)SecurityUtils.getCurrentUser().getPrincipal();
+                if (principal instanceof KeycloakPrincipal) {
+                    KeycloakPrincipal<KeycloakSecurityContext> kPrincipal = (KeycloakPrincipal<KeycloakSecurityContext>) principal;
+                    String username = kPrincipal.getKeycloakSecurityContext().getIdToken().getPreferredUsername();
+                    log.info( "{} {} {} {} {} ", username,request.getMethod(),request.getRequestURI(),request.getQueryString(),request.getRemoteAddr());
+                }
             }
         }
         return true;
