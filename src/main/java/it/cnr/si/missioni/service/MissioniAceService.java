dfc3349188fe1f071a4bde5b664827342a5cec1f
marco.spasiano <marco.spasiano@cnr.it>
2022-05-30 12:21:19 +0200

    Upgrade version of ace-client artifact

--- src/main/java/it/cnr/si/missioni/service/MissioniAceService.java
+++ src/main/java/it/cnr/si/missioni/service/MissioniAceService.java
@@ -131,7 +131,7 @@ public class MissioniAceService {
     @Cacheable(value = Costanti.NOME_CACHE_DATI_ACCOUNT)
     public UserInfoDto getAccountFromSiper(String currentLogin) {
         logger.info("getAccountFromSiper: " + currentLogin);
-        UserInfoDto userInfoDto = aceService.getUserInfoDto(currentLogin);
+        UserInfoDto userInfoDto = aceService.getUserInfoDto(currentLogin, false);
         return userInfoDto;
     }
 
