75857b2ac1c634170e9570e1346f9d2c8b6b72bd
davide.mirra <davide.mirra@iss.it>
2023-11-23 12:39:53 +0100

    commit

--- src/main/webapp/scripts/ordine-missione/taxi.js
+++ src/main/webapp/scripts/ordine-missione/taxi.js
@@ -1,50 +1,114 @@
-        'use strict';
+'use strict';
 
-missioniApp.factory('TaxiService', function ($resource) {
-        return $resource('api/rest/ordineMissione/taxi/:ids', {}, {
-            'confirm':  { method: 'PUT', params:{confirm:true}}
-        });
+missioniApp.factory('TaxiOrdineMissioneService', function ($http) {
+        return {
+            findTaxi: function(idMissione) {
+                var promise = $http.get('api/rest/ordineMissione/taxi/get', {params: {idMissione: idMissione}}).then(function (response) {
+                    return response.data;
+                });
+                return promise;
+            },
+            findSpostamenti: function(idTaxiOrdineMissione) {
+                var promise = $http.get('api/rest/ordineMissione/taxi/getSpostamenti', {params: {idTaxiOrdineMissione: idTaxiOrdineMissione}}).then(function (response) {
+                    return response.data;
+                });
+                return promise;
+            }
+        }
     });
 
-missioniApp.controller('TaxiOrdineMissioneController', function ($scope, $rootScope, $location, $routeParams, $sessionStorage, $http, $filter, TaxiService, ElencoOrdiniMissioneService, ui, COSTANTI, AccessToken, DateService) {
-    
+missioniApp.controller('TaxiOrdineMissioneController', function ($scope, $rootScope, $location, $routeParams, $sessionStorage, $http, $filter, AccessToken, AutoProprieService, DatiPatenteServiceUser, TaxiOrdineMissioneService, ElencoOrdiniMissioneService, ui, COSTANTI, DateService) {
+
     $scope.validazione = $routeParams.validazione;
     $scope.idOrdineMissione = $routeParams.idOrdineMissione;
     $scope.accessToken = AccessToken.get();
     $scope.accountModel = $sessionStorage.accountWork;
 
+    $scope.aggiungiRigaSpostamento = function () {
+      $scope.addSpostamentoTaxi = true;
+      $scope.newSpostamentoTaxi = {};
+    }
+
+    $scope.changeAuto = function(taxi) {
+        if (taxi){
+            $scope.taxiOrdineMissioneModel.targa = taxi.targa;
+            $scope.taxiOrdineMissioneModel.cartaCircolazione = taxi.cartaCircolazione;
+            $scope.taxiOrdineMissioneModel.marca = taxi.marca;
+            $scope.taxiOrdineMissioneModel.modello = taxi.modello;
+            $scope.taxiOrdineMissioneModel.polizzaAssicurativa = taxi.polizzaAssicurativa;
+        }
+    }
+
+    $scope.confirmDeleteSpostamenti = function (index) {
+        var spostamentoDaEliminare = $scope.spostamentiTaxi[index];
+        ui.confirmCRUD("Confermi l'eliminazione dello spostamento da  "+spostamentoDaEliminare.percorsoDa+" a "+spostamentoDaEliminare.percorsoA+"?", deleteSpostamenti, index);
+    }
+
     $scope.confirmDelete = function () {
-        ui.confirmCRUD("Confermi l'eliminazione della richiesta di taxi per l'ordine di missione numero "+$scope.taxiOrdineMissioneModel.ordineMissione.numero+" del "+$filter('date')($scope.taxiOrdineMissioneModel.ordineMissione.dataInserimento, COSTANTI.FORMATO_DATA)+"?", deleteTaxi);
+        ui.confirmCRUD("Confermi l'eliminazione della richiesta dell'auto propria per l'ordine di missione numero "+$scope.taxiOrdineMissioneModel.ordineMissione.numero+" del "+$filter('date')($scope.taxiOrdineMissioneModel.ordineMissione.dataInserimento, COSTANTI.FORMATO_DATA)+"?", deleteTaxi);
+    }
+
+    $scope.autoProprie = AutoProprieService.get($scope.accountModel.login);
+
+    $scope.editSpostamento= function (spostamento) {
+      spostamento.editing = true;
+    }
+
+    var undoEditingSpostamento = function (spostamento) {
+      delete spostamento.editing;
+    }
+
+    $scope.undoSpostamento = function (spostamento) {
+      undoEditingSpostamento(spostamento);
+    }
+
+    $scope.edit= function () {
+      $scope.editing = true;
+    }
+
+    var undoEditing = function () {
+      $scope.editing = false;
+    }
+
+    $scope.undo = function () {
+      undoEditing();
+    }
+
+    $scope.isDisabilitataModificaAuto = function (){
+        if ($scope.taxiOrdineMissioneModel && $scope.taxiOrdineMissioneModel.id && !$scope.editing){
+            return true;
+        }
+        return false;
     }
 
     var inizializzaDati = function(){
+        DatiPatenteServiceUser.get($scope.accountModel.login).$promise.then(function(datiPatente){
+            $scope.taxiOrdineMissioneModel = {numeroPatente:datiPatente.numero, dataRilascioPatente:datiPatente.dataRilascio,
+                    dataScadenzaPatente:datiPatente.dataScadenza, entePatente:datiPatente.ente};
+
             ElencoOrdiniMissioneService.findById($scope.idOrdineMissione).then(function(data){
-                $scope.taxiOrdineMissioneModel = {ordineMissione:data};
+                $scope.taxiOrdineMissioneModel.ordineMissione = data;
             });
+        });
     }
 
     $http.get('api/rest/ordineMissione/taxi/get', {params: {idMissione: $scope.idOrdineMissione}}).then(function (response) {
         var datiTaxiOrdineMissione = response.data;
         if (datiTaxiOrdineMissione.id === undefined){
             inizializzaDati();
-            $scope.isOrdineMissioneConfermato = false;
         } else {
             $scope.taxiOrdineMissioneModel = datiTaxiOrdineMissione;
-            $scope.viewAttachments($scope.taxiOrdineMissioneModel.id);
-            if ($scope.taxiOrdineMissioneModel.ordineMissione.stato==='CON'){
-                $scope.isOrdineMissioneConfermato = true;
-            } else {
-                $scope.isOrdineMissioneConfermato = false;
-            }
+            $http.get('api/rest/ordineMissione/taxi/getSpostamenti', {params: {idTaxiOrdineMissione: $scope.taxiOrdineMissioneModel.id}}).then(function (response) {
+                $scope.spostamentiTaxi = response.data;
+            });
         }
     });
 
-    $scope.save = function () {
+   $scope.save = function () {
             $rootScope.salvataggio = true;
             if ($scope.taxiOrdineMissioneModel.id){
                 $http.put('api/rest/ordineMissione/taxi/modify', $scope.taxiOrdineMissioneModel).success(function(data){
                     $rootScope.salvataggio = false;
-                    $scope.viewAttachments($scope.taxiOrdineMissioneModel.id);
                 }).error(function (data) {
                     $rootScope.salvataggio = false;
                 });
@@ -52,36 +116,20 @@ missioniApp.controller('TaxiOrdineMissioneController', function ($scope, $rootSc
                 $http.post('api/rest/ordineMissione/taxi/create', $scope.taxiOrdineMissioneModel).success(function(data){
                     $rootScope.salvataggio = false;
                     $scope.taxiOrdineMissioneModel = data;
-                    $scope.taxiOrdineMissioneModel.isFireSearchAttachments = false;
                 }).error(function (data) {
                     $rootScope.salvataggio = false;
                 });
             }
+            undoEditing();
     }
 
-    $scope.confirm = function () {
-        ui.confirmCRUD("Si sta per rendere definitiva la richiesta di taxi di " + $scope.taxiOrdineMissioneModel.importo+" per l'Ordine di Missione Numero: "+$scope.taxiOrdineMissioneModel.ordineMissione.numero+" del "+$filter('date')($scope.taxiOrdineMissioneModel.ordineMissione.dataInserimento, COSTANTI.FORMATO_DATA)+". L'operazione avvierà il processo di autorizzazione e la richiesta di taxi non sarà più modificabile. Si desidera Continuare?", confirmTaxi);
-    }
-
-    var confirmTaxi = function () {
+    var deleteSpostamenti = function (index) {
+        var idSpostamento = $scope.spostamentiTaxi[index].id;
             $rootScope.salvataggio = true;
-            TaxiService.confirm($scope.taxiOrdineMissioneModel,
-                    function (responseHeaders) {
-                        $rootScope.salvataggio = false;
-                    },
-                    function (httpResponse) {
-                        $rootScope.salvataggio = false;
-                    }
-            );
-    }
-
-    var deleteTaxi = function () {
-        var idTaxi = $scope.taxiOrdineMissioneModel.id;
-            $rootScope.salvataggio = true;
-            $http.delete('api/rest/ordineMissione/taxi/' + idTaxi).success(
+            $http.delete('api/rest/ordineMissione/taxi/spostamenti/' + idSpostamento).success(
                     function (data) {
                         $rootScope.salvataggio = false;
-                        inizializzaDati();
+                        $scope.spostamentiTaxi.splice(index,1);
                     }).error(
                     function (data) {
                         $rootScope.salvataggio = false;
@@ -89,53 +137,64 @@ missioniApp.controller('TaxiOrdineMissioneController', function ($scope, $rootSc
             );
     }
 
+    var deleteTaxi = function () {
+        var idTaxi = $scope.taxiOrdineMissioneModel.id;
+                        $rootScope.salvataggio = true;
+            $http.delete('api/rest/ordineMissione/taxi/' + idTaxi).success(
+                    function (data) {
+                        $rootScope.salvataggio = false;
+                        inizializzaDati();
+                        $scope.spostamentiTaxi = [];
+                        undoEditing();
+                    }).error(
+                    function (data) {
+                        $rootScope.salvataggio = false;
+                    }
+            );
+    }
+
+    var annullaDatiNuovaRiga = function () {
+      delete $scope.addSpostamentoTaxi;
+      delete $scope.newSpostamentoTaxi;
+      delete $scope.error;
+    }
+
+    $scope.undoAddSpostamentoTaxi = function () {
+        annullaDatiNuovaRiga();
+    }
+
+    $scope.insertSpostamentoTaxi = function (newRigaSpostamento) {
+        newRigaSpostamento.ordineMissioneTaxi = $scope.taxiOrdineMissioneModel;
+            $rootScope.salvataggio = true;
+            $http.post('api/rest/ordineMissione/taxi/createSpostamento', newRigaSpostamento).success(function(data){
+                    $rootScope.salvataggio = false;
+                    if (!$scope.spostamentiTaxi){
+                        $scope.spostamentiTaxi = [];
+                    }
+                    $scope.spostamentiTaxi.push(data);
+                    $scope.undoAddSpostamentoTaxi();
+            }).error(function (data) {
+                $rootScope.salvataggio = false;
+            });
+    }
+
+    $scope.modifySpostamento = function (spostamentoTaxi) {
+        $rootScope.salvataggio = true;
+        $http.put('api/rest/ordineMissione/taxi/modifySpostamento', spostamentoTaxi).success(function(data){
+            $rootScope.salvataggio = false;
+            undoEditingSpostamento(spostamentoTaxi);
+        }).error(function (data) {
+            $rootScope.salvataggio = false;
+        });
+    }
+
+    $scope.ricerca = function () {
+        TaxiOrdineMissioneService.findMissioni($scope.cdsRich, $scope.daNumero, $scope.aNumero).then(function(data){
+            $scope.ordiniMissione = data;
+        });
+    }
+
     $scope.previousPage = function () {
       parent.history.back();
-    }
-
-    $scope.confirmDeleteAttachment = function (attachment, idOrdineMissione) {
-        ui.confirmCRUD("Confermi l'eliminazione del file "+attachment.nomeFile+"?", deleteAttachment, attachment);
-    }
-
-    var deleteAttachment = function (attachment) {
-        $rootScope.salvataggio = true;
-        var x = $http.delete('api/rest/ordine/deleteAttachment?id=' + attachment.id+'&idOrdine=' + $routeParams.idOrdineMissione);
-        var y = x.then(function (result) {
-            var attachments = $scope.taxiOrdineMissioneModel.attachments;
-            if (attachments && Object.keys(attachments).length > 0){
-                var newAttachments = attachments.filter(function(el){
-                    return el.id !== attachment.id;
-                });
-                $scope.taxiOrdineMissioneModel.attachments = newAttachments;
-                if (Object.keys(newAttachments).length = 0){
-                    $scope.taxiOrdineMissioneModel.attachmentsExists = false;
-                }
-            }
-            $rootScope.salvataggio = false;
-            ui.ok();
-        });
-        x.error(function (data) {
-            $rootScope.salvataggio = false;
-        });
-    }
-
-    $scope.viewAttachments = function (idTaxi) {
-        if (!$scope.taxiOrdineMissioneModel.isFireSearchAttachments){
-            $http.get('api/rest/ordineMissione/taxi/viewAttachments/' + idTaxi).then(function (data) {
-                $scope.taxiOrdineMissioneModel.isFireSearchAttachments = true;
-                var attachments = data.data;
-                if (attachments && Object.keys(attachments).length > 0){
-                    $scope.attachmentsExists = true;  
-                } else {
-                    $scope.attachmentsExists = false;
-                }
-                $scope.taxiOrdineMissioneModel.attachments = attachments;
-            }, function () {
-                $scope.taxiOrdineMissioneModel.isFireSearchAttachments = false;
-                $scope.taxiOrdineMissioneModel.attachmentsExists = false;
-                $scope.taxiOrdineMissioneModel.attachments = {};
-            });
-        }
-//        $scope.taxiOrdineMissioneModel.viewAttachment = true;
     }
 });
