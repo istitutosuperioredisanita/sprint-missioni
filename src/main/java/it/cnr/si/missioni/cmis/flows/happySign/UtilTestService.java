6249dcb607e27bcc6ef6967d174827e26d2cf21e
davide.mirra <davide.mirra@iss.it>
2024-10-24 12:15:55 +0200

    utiltest ordinemissione

--- src/main/java/it/cnr/si/missioni/cmis/flows/happySign/UtilTestService.java
+++ src/main/java/it/cnr/si/missioni/cmis/flows/happySign/UtilTestService.java
@@ -35,8 +35,8 @@ public class UtilTestService {
         StartWorflowDto startInfo= new StartWorflowDto();
         startInfo.setTemplateName("duilio_app");
 
-        startInfo.addSigner("davide.mirra@iss.it");
-        startInfo.addSigner("simona.fortunato@iss.it");
+        startInfo.addSigner(ordineMissione.getUid());
+        startInfo.addSigner(ordineMissione.getUid());
 
         File f = new File();
         f.setFilename(missioniCMISService.parseFilename(modulo.getKey()));
